944867b288f32eb8dbb0762fd53ae964076bf24a
Ellen Woodward <S00222321@atu.ie>
2023-02-07 16:03:21 +0000

    Created and displayed movie array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-list';
+
+  movieList = [
+    { id:1, title: 'The Godfather I', year: '1972', director: 'Francis Ford Coppola'},
+    { id:2, title: 'The Godfather II', year: '1978', director: 'Francis Ford Coppola'},
+    { id:3, title: 'The Godfather III', year: '1982', director: 'Francis Ford Coppola'}
+
+  ]
 }
